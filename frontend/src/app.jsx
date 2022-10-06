d50a6d3d1096783ef3e4c9d29c0d6c8d1c88e2b6
tannerjs7 <tannerschilperoort@gmail.com>
2022-10-05 22:27:50 -0400

    Added to the front end

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home, PageNotFound } from './pages'
+import "./index.css"
+// import { AppNav } from './components/Nav'
+
 export const App = () => {
     return (
-        
+        <BrowserRouter>
+            {/* <AppNav /> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <footer>Footer!</footer>
+        </BrowserRouter>
     )
 }
---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+export const PageNotFound = () => {
+    return (
+        <h1>Try another URL</h1>
+    )
+}
